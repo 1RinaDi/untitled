474cd570ca58a42161f5e6836eac8a457cafe0d6
1RinaDi <63125292+1RinaDi@users.noreply.github.com>
2020-05-27 15:59:25 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-echo date('j') . "-" . date('F') . "-" . date('Y');
-echo "<br>Дибаева К.Р. ПР317";
-?>
+function getAge($y, $m, $d) {
+    if($m > date('m') || $m == date('m') && $d >date('d'))
+        return (date('Y') - $y - 1);
+    else
+        return (date('Y') - $y);
+}
+echo(getAge(2002,5,25));
